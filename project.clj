5bba9384938a69d09d21eba47c3fc2ad0872303a
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-25 20:48:41 +0300

    Add base project files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-wiite "0.1.0"
+  :description "Clojure library for using database backed Atom"
+  :url "https://github.com/stormaaja/clj-wiite"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
