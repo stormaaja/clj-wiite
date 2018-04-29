cf7a9791b17b133e6828d7b3614c92584a7be68f
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-29 08:50:15 +0300

    Add function for creating watom

--- src/clj_wiite/core.clj
+++ src/clj_wiite/core.clj
@@ -1,1 +1,6 @@
-(ns clj-wiite.core)
+(ns clj-wiite.core
+  (:require [clj-wiite.watom :refer [create-watom]]))
+
+(defn watom
+  ([store x & options] (apply create-watom store x options))
+  ([store x] (create-watom store x :load-state? true)))
