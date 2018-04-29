c6f5381bbb1a7e2fb909c7decc68d0c9d6541c17
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-29 09:11:09 +0300

    Add file exists check

--- src/clj_wiite/file_store.clj
+++ src/clj_wiite/file_store.clj
@@ -1,14 +1,19 @@
 (ns clj-wiite.file-store
   (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clj-wiite.store :refer [Store]]))
+
+(defn- file-exists? [path]
+  (.exists (io/as-file path)))
 
 (defrecord FileStore [file]
   Store
   (write-state! [store state]
     (spit (:file store) (str state)))
   (load-state [store]
-    (let [v (slurp (:file store))]
-      (edn/read-string v))))
+    (when (file-exists? (:file store))
+      (let [v (slurp (:file store))]
+        (edn/read-string v)))))
 
 (defn file-store [file]
   (FileStore. file))
