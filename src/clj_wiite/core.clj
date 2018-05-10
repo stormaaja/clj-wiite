800b090124ae9052edad26257c3f9fc5d7bcc44f
Matti Ahinko <matti.ahinko@gmail.com>
2018-05-10 21:48:42 +0300

    Remove optional parameters

--- src/clj_wiite/core.clj
+++ src/clj_wiite/core.clj
@@ -5,8 +5,5 @@
   ^{:added "0.1.0"
     :doc "Create Clojure Atom with watch with two operations:
            - Changed Atom state is being written to store
-           - Initial state will be loaded from store
-          Optional parameter (options) can be
-           - :load-state? Wheter load state from store or use given value x
-           - :store-key Key to give for store and to use with atom"}
-  [store x & options] (apply create-watom store x options))
+           - Initial state will be loaded from store"}
+  [store] (create-watom store))
