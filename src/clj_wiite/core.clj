f313a5176d93f6530f468ffffd525e3414a2b815
Matti Ahinko <matti.ahinko@gmail.com>
2018-05-29 22:33:35 +0300

    Update core watom function

--- src/clj_wiite/core.clj
+++ src/clj_wiite/core.clj
@@ -1,9 +1,11 @@
 (ns clj-wiite.core
-  (:require [clj-wiite.watom :refer [create-watom]]))
+  (:require [clj-wiite.watom :refer [create-watom]]
+            [clj-wiite.db-store :refer [db-store]]))
 
-(defn watom
+(defn watom [conn]
   ^{:added "0.1.0"
     :doc "Create Clojure Atom with watch with two operations:
-           - Changed Atom state is being written to store
-           - Initial state will be loaded from store"}
-  [store] (create-watom store))
+           - Changed Atom state is being written to DB store
+           - Initial state will be loaded from DB store
+          DB store will be initialized with parameters given."}
+  (create-watom (db-store conn)))
